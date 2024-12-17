894c29cf975af446b1699697e8a1dc249fbfa7b8
CorNit0 <ksyshayt@gmail.com>
2024-12-17 17:57:12 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+Name = "Alex"
+print("Name: " + Name)
+Age = 18
+print('Age: ' + str(Age))
+New_Age = 19
+print("New age: " + str(New_Age))
+Is_student = True
+print("Is Student: " + str(Is_student))
